e48938d1f7efb46d8667247fdefc33eb41ac430e
azam.wahid <azamwahid@outlook.com>
2025-07-10 02:15:26 +0500

    workiing on routing

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -14,10 +14,58 @@ router.post("/", async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
 //update
+router.put("/:id", async (req, res) => {
+    try {
+        const UpdateHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(UpdateHotel);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 //delete
+router.delete("/:id", async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Delete Successfull");
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 //get
+router.get("/:id", async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id)
+        res.status(200).json(hotel);
+
+    } catch (err) {
+        // res.status(500).json(err);
+        next(err);
+    }
+});
+
 //get all
+router.get("/", async (req, res, next) => {
+
+    const failed = true;
+    const err = new Error();
+    err.status = 404
+    err.message = "sorry not found";
+    if (failed) return next(err);
+
+    try {
+        const hotels = await Hotel.findById("sass")
+        res.status(200).json(hotels);
+
+    } catch (err) {
+        next(err);
+    }
+});
 
 export default router;
