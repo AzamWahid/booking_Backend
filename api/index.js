0e48d1b883e44e3ec07e8feba06ce567319f4633
Muhammad Azam <AzamWahid@outlook.com>
2025-07-09 20:42:22 +0500

    routing add

---
+++ api/index.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import authRoute from './routes/auth.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+import usersRoute from './routes/users.js'
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        const con = await mongoose.connect(process.env.MANGO);
+        console.log('connected to mangoDB')
+        console.log(`MongoDB Connected: ${con.connection.host}`);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+mongoose.connection.on('disconnected', () => {
+    console.log('disconnected')
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('connected')
+});
+
+app.use(express.json());
+
+app.use('/api/auth',authRoute);
+app.use('/api/users',usersRoute);
+app.use('/api/hotels',hotelsRoute);
+app.use('/api/rooms',roomsRoute);
+
+app.listen(8800, () => {
+    connect();
+    console.log("backend connected");
+})
---
+++ api/routes/hotels.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import Hotel from '../models/Hotel.js';
+
+const router = express.Router();
+
+
+//Create
+router.post("/", async (req, res) => {
+    const newHotel = new Hotel(req.body)
+    try {
+        const saveHotel = await newHotel.save()
+        res.status(200).json(saveHotel);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+//update
+//delete
+//get
+//get all
+
+export default router;
