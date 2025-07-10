8dc5f94889df5c6acb467b4e9335bfa218398e31
azam.wahid <azamwahid@outlook.com>
2025-07-11 02:29:32 +0500

    1:13

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,71 +1,25 @@
 import express from 'express'
 import Hotel from '../models/Hotel.js';
+import { createError } from '../utils/error.js';
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 
 //Create
-router.post("/", async (req, res) => {
-    const newHotel = new Hotel(req.body)
-    try {
-        const saveHotel = await newHotel.save()
-        res.status(200).json(saveHotel);
-
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+router.post("/", verifyAdmin, createHotel);
 
 //update
-router.put("/:id", async (req, res) => {
-    try {
-        const UpdateHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
-        res.status(200).json(UpdateHotel);
-
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+router.put("/:id", verifyAdmin, updateHotel);
 
 //delete
-router.delete("/:id", async (req, res) => {
-    try {
-        await Hotel.findByIdAndDelete(req.params.id)
-        res.status(200).json("Delete Successfull");
-
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+router.delete("/:id", verifyAdmin, deleteHotel);
 
 //get
-router.get("/:id", async (req, res) => {
-    try {
-        const hotel = await Hotel.findById(req.params.id)
-        res.status(200).json(hotel);
-
-    } catch (err) {
-        // res.status(500).json(err);
-        next(err);
-    }
-});
+router.get("/:id", getHotel);
 
 //get all
-router.get("/", async (req, res, next) => {
-
-    const failed = true;
-    const err = new Error();
-    err.status = 404
-    err.message = "sorry not found";
-    if (failed) return next(err);
-
-    try {
-        const hotels = await Hotel.findById("sass")
-        res.status(200).json(hotels);
-
-    } catch (err) {
-        next(err);
-    }
-});
+router.get("/", getHotels);
 
 export default router;
