3a882d89a2ce9d536f4d38efcc61d28d7cd21ff8
Muhammad Azam <AzamWahid@outlook.com>
2025-07-11 15:14:46 +0500

    working

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,22 @@
 import express from 'express'
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/rooms.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+//Create
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+//update
+router.put("/:id", verifyAdmin, updateRoom);
+
+//delete
+router.delete("/:id", verifyAdmin, deleteRoom);
+
+//get
+router.get("/:id", getRoom);
+
+//get all
+router.get("/", getRooms);
 
 export default router;
